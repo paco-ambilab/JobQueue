5d7dd00902d5ac713954bd698c02b89ac40defc8
paco <paco89lol@gmail.com>
2020-06-06 17:32:36 +0800

    Update Tests.swift

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -13,18 +13,6 @@ class Tests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
     func testRetainSelf() {
         
         let expectation = XCTestExpectation(description: "")
